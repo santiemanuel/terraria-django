6bdc8c3be68acbe1fad95b9d703ec4fbd77cd0d0
santiemanuel <santiemanuel@gmail.com>
2023-12-07 17:55:09 -0300

    Corregido MEDIA ROOT

--- terraria/settings.py
+++ terraria/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.UserModel"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
